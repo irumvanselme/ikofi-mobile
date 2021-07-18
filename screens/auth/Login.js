d65e6dc8fac71df65b289b4e78a99b4b86ed0f4c
irumvanselme <andesanselme@gmail.com>
2021-07-18 13:06:18 +0200

    Create the Auth Screens

---
+++ screens/auth/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function LoginScreen(){
+    return (
+        <View>
+            <Text>
+                I am the log in screen
+            </Text>
+        </View>
+    )
+}
---
+++ screens/auth/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function RegisterScreen(){
+    return (
+        <View>
+            <Text>
+                I am the register screen
+            </Text>
+        </View>
+    )
+}
