c5eed7acce035bec76f2ffbfb75775b3b517e968
irumvanselme <andesanselme@gmail.com>
2021-07-18 14:56:50 +0200

    Made the Login screen

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -3,12 +3,14 @@ import { Text, View } from "react-native";
 import Screen from "../../layouts/Screen";
 import AuthScreenLayout from "../../layouts/AuthScreen";
 import FormControl from "../../components/FormControl";
+import ButtonComponent from "../../components/ButtonComponent";
 
 export default function LoginScreen() {
     return (
         <AuthScreenLayout title="Log in into Ikofi">
             <FormControl label="Email, username or Mobile" placeholder="Email, username, phone"/>
             <FormControl label="Password" placeholder="password" password/>
+            <ButtonComponent>Log in</ButtonComponent>
         </AuthScreenLayout>
     )
 }
