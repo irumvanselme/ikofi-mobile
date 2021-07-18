8aa36b3fdee69f568d14383821e46f69342d4c5b
irumvanselme <andesanselme@gmail.com>
2021-07-18 11:55:40 +0200

    Fix some Home screen component

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,44 @@
 import React from "react";
-import { StyleSheet, Text, ImageBackground, View, Image } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Screen from "../layouts/Screen";
+import Logo from "../components/logo/Logo";
 import { colors } from "../constants/colors";
 
 export default function () {
     return (
         <Screen>
-            <Image source={require("../assets/logo.png")} resizeMode="contain" style={{ backgroundColor: "red"}}/>
-            <Text>Holla I am anselme</Text>
+            <View style={styles.container}>
+                <Logo/>
+                <View  style={styles.texts}>
+                    <Text style={styles.header}>IKOFI</Text>
+                    <Text style={styles.header}>Welcome back !</Text>
+                    <Text style={styles.slogan}>Send Money without tears</Text>
+                </View>
+                <View>
+                
+                </View>
+            </View>
         </Screen>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 100
+    },
+    texts: {
+        marginTop: 50
+    },
+    header: {
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 30
+    },
+    slogan: {
+        textAlign: "center",
+        marginTop: 30,
+        color: colors.primary
+    }
+})
