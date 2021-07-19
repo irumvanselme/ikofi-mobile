0210f19dd73020a6e7e7f6bfea181d778b5617d4
irumvanselme <andesanselme@gmail.com>
2021-07-19 14:02:49 +0200

    Added an an app navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import DashboardScreen from "../screens/dashboard/Dashboard";
+import ProfileScreen from "../screens/auth/Profile";
+import SendMoneyScreen from "../screens/money/Send";
+import navigationTheme from "./navigationTheme";
+
+const Tabs = createBottomTabNavigator();
+
+const TabNavigator = () => (
+	<Tabs.Navigator tabBarOptions={{}}>
+		<Tabs.Screen
+			options={{
+				tabBarIcon: ({ size, color }) => (
+					<MaterialCommunityIcons
+						name="home"
+						size={size}
+						color={color}
+					/>
+				),
+			}}
+			name="Home"
+			component={DashboardScreen}
+		/>
+		<Tabs.Screen name="Send" component={SendMoneyScreen} />
+		<Tabs.Screen name="Profile" component={ProfileScreen} />
+	</Tabs.Navigator>
+);
 
 export default function AppNavigator() {
 	return (
-		<View>
-			<Text> I am the app navigator </Text>
-		</View>
+		<NavigationContainer theme={navigationTheme}>
+			<TabNavigator />
+		</NavigationContainer>
 	);
 }
---
+++ screens/money/Send.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Screen from "../../layouts/Screen";
+
+export default function SendMoneyScreen() {
+	return (
+		<Screen>
+			<View>
+				<Text>Send Money screen</Text>
+			</View>
+		</Screen>
+	);
+}
